4e57e3a7a6beb6c1b35634b633a11c61ec865048
chiUwU <97713639+PhamYenChi3@users.noreply.github.com>
2022-06-05 22:20:57 +0700

    first commit

---
+++ t2108a/ValidateAccount.java
@@ -0,0 +1,20 @@
+package bank.t2108a;
+
+public class ValidateAccount {
+    public final String MOBILE = "0904999999";
+    public final String PASS = "cham` kem?";
+    public boolean checkAccount( String mobile, String pass){
+        if(mobile.equals(MOBILE)){
+            if(pass.equals(PASS)){
+                System.out.println("dang nhap thanh cong");
+                return true;
+            }else {
+                System.out.println("sai mat khau, dang nhap lai i");
+                return false;
+            }
+        }else{
+            System.out.println("kiem tra sdt hoac mk");
+            return false;
+        }
+    }
+}
